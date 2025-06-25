fe18eda03c36ad41d9f3c892f4fc355bffc18aa3
Nouman.Moeen-EXT@lebara.com <noumanmoeen@192.168.1.8>
2025-06-25 18:09:55 +0500

    removed code

--- android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
+++ android/src/main/java/com/reactnativesimcardsmanager/SimCardsManagerModule.java
@@ -20,7 +20,6 @@ import android.telephony.SubscriptionManager;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
-import android.app.Activity;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -157,43 +156,36 @@ public class SimCardsManagerModule extends ReactContextBaseJavaModule {
     }
     return;
   }
+
   @RequiresApi(api = Build.VERSION_CODES.P)
-private void handleResolvableError(Promise promise, Intent intent) {
+  private void handleResolvableError(Promise promise, Intent intent) {
+        Log.d("SimCardsManager", "Resolving eSIM error"); // Logcat log
+
     try {
-       Log.d("SimCardsManager", "Resolving eSIM error"); // Logcat log
-        int resolutionRequestCode = 3;
-        PendingIntent callbackIntent = intent.getParcelableExtra("android.telephony.euicc.extra.RESOLUTION_INTENT");
-        
-        if (callbackIntent != null) {
-                  Log.d("SimCardsManager", "Started resolution activity"); // Logcat log
-            mReactContext.addActivityEventListener(new BaseActivityEventListener() {
-                @Override
-                public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-                    if (requestCode == resolutionRequestCode) { // Check request code
-                        if (resultCode == Activity.RESULT_OK) {
-                            // eSIM successfully installed
-                            promise.resolve("eSIM installation successful");
-                        } else {
-                            // eSIM installation failed or was canceled
-                            promise.reject("4", "eSIM installation failed or was canceled");
-                        }
-                        // Remove event listener after handling the result
-                        mReactContext.removeActivityEventListener(this);
-                    }
-                }
-            });
+      // Resolvable error, attempt to resolve it by a user action
+      // FIXME: review logic of resolve functions
+      int resolutionRequestCode = 3;
+      PendingIntent callbackIntent = PendingIntent.getBroadcast(
+        mReactContext,
+        resolutionRequestCode,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE
+      );
 
-            // Start the resolution process
-            callbackIntent.send(resolutionRequestCode);
-        } else {
-            promise.reject("NO_RESOLUTION_INTENT", "No resolution intent available.");
-        }
-    } catch (PendingIntent.CanceledException e) {
-                  Log.e("SimCardsManager", "Error resolving eSIM error", e); // Logcat error log
+      if (callbackIntent != null) {
+        mEsimModule.getMgr().startResolutionActivity(mReactContext.getCurrentActivity(), resolutionRequestCode, intent, callbackIntent);
+        Log.d("SimCardsManager", "Started resolution activity"); // Logcat log
+      } else {
+        Log.e("SimCardsManager", "No resolution intent available"); // Logcat error log
+        promise.reject("NO_RESOLUTION_INTENT", "No resolution intent available.");
+      }
+    } catch (Exception e) {
+            Log.e("SimCardsManager", "Error resolving eSIM error", e); // Logcat error log
 
-        promise.reject("INTENT_CANCELED", "Resolution intent was canceled.", e);
+      promise.reject("3", "EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR - Can't setup eSim due to Activity error "
+          + e.getLocalizedMessage());
     }
-}
+  }
 
   private boolean checkCarrierPrivileges() {
     TelephonyManager telManager = (TelephonyManager) mReactContext.getSystemService(Context.TELEPHONY_SERVICE);
